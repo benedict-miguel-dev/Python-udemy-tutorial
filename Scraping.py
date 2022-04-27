54e82cf978c4549aefe4e271e64c76298f730e83
Desktop <benedictmigueldev@gmail.com>
2022-04-27 13:16:17 -0400

    Added scraping Files

---
+++ Scraping.py
@@ -0,0 +1,16 @@
+from random import betavariate
+from turtle import title
+from webbrowser import get
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get('https://www.imdb.com/chart/top/')
+soup = BeautifulSoup(r.text, 'html.parser')
+
+# lists = soup.findAll(class_= 'product_pod')
+# Using Selector
+movies = soup.select('tr')
+movies.pop(0)
+print(movies[0])
+# for num, item in enumerate(movies):
+#     print(num)
