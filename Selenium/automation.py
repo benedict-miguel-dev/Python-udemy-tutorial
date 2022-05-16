44a073978fc5c862d892b8fa10f5f528bd070492
miguel-benedict-216237364 <ben106@my.yorku.ca>
2022-05-15 21:05:18 -0400

    macbook changes

--- Selenium/automation.py
+++ Selenium/automation.py
@@ -9,3 +9,8 @@ chrome_browser = webdriver.Chrome(service=Service(
     ChromeDriverManager().install()), options=chrome_options)
 chrome_browser.maximize_window()
 chrome_browser.get('https://demo.seleniumeasy.com/basic-first-form-demo.html')
+print('Selenium Easy Demo' in chrome_browser.title)
+# Closes the initial warm up message
+close = chrome_browser.find_element_by_partial_link_text('No, thanks')
+
+print(close)
