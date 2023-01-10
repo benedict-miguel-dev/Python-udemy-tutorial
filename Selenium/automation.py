d42cab54735782aea1342ee16d8e7f1da995f385
miguel-benedict-216237364 <ben106@my.yorku.ca>
2023-01-09 20:45:41 -0500

    modified some code

--- Selenium/automation.py
+++ Selenium/automation.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+import time
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 timeout = 5
@@ -11,15 +12,19 @@ chrome_browser = webdriver.Chrome(service=Service(
     ChromeDriverManager().install()), options=chrome_options)
 #  chrome_browser.maximize_window()
 # Go to the website
-chrome_browser.get('https://www.amazon.ca/')
-# Make sure that we are in the website 
-assert 'Amazon.ca: Low Prices – Fast Shipping – Millions of Items' in chrome_browser.title
-# Get the search bar and clear the search bar
-search_bar = chrome_browser.find_element(By.ID, 'twotabsearchtextbox')
-search_bar.clear()
-# Get the search button
-search_button =  chrome_browser.find_element(By.ID, 'nav-search-submit-button')  
-# Set the text 
-search_bar.send_keys('Sending Keys')
-# Simulate click search 
-search_button.click()
+chrome_browser.get('https://quickfs.net/company/TSLA:US')
+# Make sure that we are in the website
+ks = chrome_browser.find_element(by=By.CLASS_NAME, value='ks')
+print(ks.get_attribute('innerHTML'))
+
+chrome_browser.quit()
+# assert 'Amazon.ca: Low Prices – Fast Shipping – Millions of Items' in chrome_browser.title
+# # Get the search bar and clear the search bar
+# search_bar = chrome_browser.find_element(By.ID, 'twotabsearchtextbox')
+# search_bar.clear()
+# # Get the search button
+# search_button =  chrome_browser.find_element(By.ID, 'nav-search-submit-button')
+# # Set the text
+# search_bar.send_keys('Sending Keys')
+# # Simulate click search
+# search_button.click()
